03edb02929293d0ff28eea98b57935102b0109fb
Ahmed Waheed <aw0reborn@gmail.com>
2017-12-02 14:25:31 +0500

    uploaded

---
+++ get_area.py
@@ -0,0 +1,17 @@
+## IMPORTS GO HERE
+from math import pi
+## END OF IMPORTS 
+
+
+#### YOUR CODE FOR get_area GOES HERE ####
+def get_area(x):
+    return(pi*x*x)
+#### End OF MARKER
+
+
+#### YOUR CODE FOR output_parameter GOES HERE ####
+def output_parameter(x):
+    print'The parameter of the circle with radius', x , 'is' , 2*pi*x
+#### End OF MARKER  
+
+
